7a8bc9a9379ac784c2605e7b13536b7dcb686645
superdom177-source <superdom177@gmail.com>
2026-07-29 09:52:50 +0200

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,1 +1,32 @@
+import { auth } from "./firebase.js";
 
+import { 
+    signInWithEmailAndPassword 
+} from "https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js";
+
+
+const loginButton = document.getElementById("loginButton");
+
+
+loginButton.addEventListener("click", () => {
+
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+
+    signInWithEmailAndPassword(auth, email, password)
+
+    .then(() => {
+
+        window.location.href = "index.html";
+
+    })
+
+    .catch((error) => {
+
+        document.getElementById("message").innerHTML =
+        "ACCESS DENIED: " + error.message;
+
+    });
+
+});
