d4bf1220ca5defefc04ea049c37b1b9cce8a7e5b
superdom177-source <superdom177@gmail.com>
2026-07-29 09:52:13 +0200

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,1 +1,20 @@
+// Import Firebase
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js";
 
+// Firebase konfigurace
+const firebaseConfig = {
+  apiKey: "AIzaSyA_6eKHjsMKbJ4l5PkhZuhyyoOLY-Ro80I",
+  authDomain: "silent-protocol-foundation.firebaseapp.com",
+  projectId: "silent-protocol-foundation",
+  storageBucket: "silent-protocol-foundation.firebasestorage.app",
+  messagingSenderId: "908197866917",
+  appId: "1:908197866917:web:8e9d9eb3676f20449d4c4c"
+};
+
+// Inicializace Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+// Export
+export { auth };
